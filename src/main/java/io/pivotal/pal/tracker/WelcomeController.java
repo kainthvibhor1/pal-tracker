d38945896067908a5ffc2345896dd4dcf6f9b980
Vibhor Kainth <vibhor.kainth@perficient.com>
2019-05-29 10:31:48 -0400

    Add environment variable configuration

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,15 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class WelcomeController {
+    private String message;
 
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message) {
+        this.message = message;
+    }
 
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return message;
     }
 
 }
